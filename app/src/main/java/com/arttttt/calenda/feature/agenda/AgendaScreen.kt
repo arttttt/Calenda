a8b96911de4d7fd115fadc0e7d9498e971fbb343
arttttt <artem-bambalov@yandex.ru>
2025-08-26 00:06:31 +0300

    basic agenda screen

--- app/src/main/java/com/arttttt/calenda/feature/agenda/AgendaScreen.kt
+++ app/src/main/java/com/arttttt/calenda/feature/agenda/AgendaScreen.kt
@@ -1,11 +1,14 @@
 package com.arttttt.calenda.feature.agenda
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
@@ -13,15 +16,31 @@ fun AgendaScreen() {
     AgendaScreenContent()
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun AgendaScreenContent() {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center,
-    ) {
-        Text(
-            text = "Agenda",
-        )
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = "Agenda",
+                    )
+                }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {}
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = null,
+                )
+            }
+        },
+    ) { innerPadding ->
+        innerPadding
     }
 }
 
